2b4d81994f643db5c22bdf3675dbb5904eb3e4a9
Mikael Batista da Trindade <mikaelbatistadatrindade@Mikaels-MacBook-Air.local>
2024-10-09 23:00:32 -0300

    classe troco

--- Code/src/br/calebe/ticketmachine/core/Troco.java
+++ Code/src/br/calebe/ticketmachine/core/Troco.java
@@ -2,46 +2,25 @@ package br.calebe.ticketmachine.core;
 
 import java.util.Iterator;
 
-/**
- *
- * @author Calebe de Paula Bianchini
- */
-class Troco {
+public class Troco {
 
     protected PapelMoeda[] papeisMoeda;
 
     public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
+        int[] notas = {100, 50, 20, 10, 5, 2};
+        papeisMoeda = new PapelMoeda[notas.length];
+
+        for (int i = 0; i < notas.length; i++) {
+            int quantidade = valor / notas[i]; // Calcula quantas cédulas dessa nota são necessárias
+            if (quantidade > 0) {
+                papeisMoeda[i] = new PapelMoeda(notas[i], quantidade);
+                valor -= quantidade * notas[i]; // Atualiza o valor restante
+            }
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
+
+        if (valor > 0) {
+            throw new IllegalArgumentException("Não foi possível fornecer troco exato.");
         }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
@@ -49,6 +28,8 @@ class Troco {
     }
 
     class TrocoIterator implements Iterator<PapelMoeda> {
+
+        private int currentIndex = 0;
 
         protected Troco troco;
 
@@ -58,29 +39,26 @@ class Troco {
 
         @Override
         public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                if (troco.papeisMoeda[i] != null) {
+            while (currentIndex < troco.papeisMoeda.length) {
+                if (troco.papeisMoeda[currentIndex] != null) {
                     return true;
                 }
+                currentIndex++;
             }
             return false;
         }
 
         @Override
         public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
+            if (hasNext()) {
+                return troco.papeisMoeda[currentIndex++];
             }
-            return ret;
+            return null;
         }
 
         @Override
         public void remove() {
-            next();
+            throw new UnsupportedOperationException("Operação não suportada.");
         }
     }
 }
